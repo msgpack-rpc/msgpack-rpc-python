654550415911148594542ea3f7b7842f9115f939
Masahiro Nakagawa <repeatedly@gmail.com>
2012-06-07 12:56:24 +0900

    Make exceptions more better pointed by seoyougjin

--- msgpackrpc/error.py
+++ msgpackrpc/error.py
@@ -1,11 +1,36 @@
 class RPCError(Exception):
-    pass
+    CODE = ".RPCError"
+
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+    @property
+    def code(self):
+        return self.__class__.CODE
+
+    def to_msgpack(self):
+        return [self.message]
+
+    @staticmethod
+    def from_msgpack(message):
+        return RPCError(message)
 
 class TimeoutError(RPCError):
+    CODE = ".TimeoutError"
     pass
 
 class TransportError(RPCError):
+    CODE = ".TransportError"
     pass
 
-class NoMethodError(RPCError):
+class CallError(RPCError):
+    CODE = ".NoMethodError"
+    pass
+
+class NoMethodError(CallError):
+    CODE = ".CallError.NoMethodError"
+    pass
+
+class ArgumentError(CallError):
+    CODE = ".CallError.ArgumentError"
     pass
