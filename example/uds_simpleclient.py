690b18f11cf017061e8d61817cc2bb104b92a098
hvishwanath <harish.shastry@gmail.com>
2013-04-08 16:23:56 +0530

    Simple msgpackrpc client and server examples using Unix domain socket support.

--- example/uds_simpleclient.py
+++ example/uds_simpleclient.py
@@ -5,6 +5,7 @@
 import msgpackrpc.udsaddress
 from msgpackrpc.transport import uds
 
+#Use UDSAddress instead of default Address object
 client = msgpackrpc.Client(msgpackrpc.udsaddress.UDSAddress("/tmp/exrpc"), builder=uds)
 result = client.call('sum', 1, 2)  # = > 
 print "Sum of 1 and 2 : %d" % result
--- example/uds_simpleserver.py
+++ example/uds_simpleserver.py
@@ -9,6 +9,8 @@ class SumServer(object):
     def sum(self, x, y):
         return x + y
 
+# Use builder as uds. default builder is tcp which creates tcp sockets
 server = msgpackrpc.Server(SumServer(), builder=uds)
+# Use UDSAddress instead of msgpackrpc.Address
 server.listen(msgpackrpc.udsaddress.UDSAddress('/tmp/exrpc'))
 server.start()
